0131da630f6f47b67726c44ea7253e0f9c1a14f0
Jack Spence <Jack@Guys-MacBook-Pro.local>
2022-02-08 10:09:37 +1000

    Added My Project

---
+++ src/components/NavButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavButton({text, url}) {
+    console.log("text:",text ) 
+    console.log("url:",url)
+    return (
+        <div>
+            
+            <button class="bg-grey-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+
+
+        </div>
+    )
+}
